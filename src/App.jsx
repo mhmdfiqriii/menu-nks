cab54f99e24eec44e5ba412250887fd6d644abe2
permenkojek <simplydesign.png@gmail.com>
2026-04-06 18:55:08 +0700

    add cart working

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
+import { useState } from "react"
 import { menu } from "./data/menu"
 
 function App() {
+  const [cart, setCart] = useState([])
+
+  const addToCart = (item) => {
+    setCart(prev => {
+      const existing = prev.find(i => i.id === item.id)
+
+      if (existing) {
+        return prev.map(i =>
+          i.id === item.id
+            ? { ...i, qty: i.qty + 1 }
+            : i
+        )
+      }
+
+      return [...prev, { ...item, qty: 1 }]
+    })
+  }
+
+  const total = cart.reduce((acc, item) => acc + item.price * item.qty, 0)
+
   return (
     <div>
       <h1>Menu NKS</h1>
@@ -9,10 +30,23 @@ function App() {
         <div key={item.id}>
           <p>{item.name}</p>
           <p>Rp. {item.price}</p>
-          <button>Tambah</button>
+          <button onClick={() => addToCart(item)}>Tambah</button>
         </div>
       ))}
 
+      <hr />
+
+      <h2>Keranjang</h2>
+
+      {cart.length === 0 && <p>Belum ada pesanan</p>}
+
+      {cart.map(item => (
+        <div key={item.id}>
+          <p>{item.name} x {item.qty}</p>
+        </div>
+      ))}
+
+      <h3>Total: Rp {total}</h3>
     </div>
   )
 }
