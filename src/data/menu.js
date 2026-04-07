9239ebeb593d64b55976fc373f833ac41e4c2ff8
permenkojek <simplydesign.png@gmail.com>
2026-04-07 19:30:04 +0700

    add multi brand system

--- src/data/menu.js
+++ src/data/menu.js
@@ -1,25 +1,65 @@
-export const menu = [
+export const brands = [
   {
-    id: 1,
-    name: "Thai Tea",
-    price: 10000,
-    category: "minuman",
-    options: {
-      Size: ["Regular", "Large"],
-      Sugar: ["Normal", "Less Sugar", "No Sugar"],
-      Ice: ["Normal Ice", "Less Ice", "No Ice"]
-    }
+    name: "Kopi Kenangan",
+    menu: [
+      {
+        id: 1,
+        name: "Kopi Kenangan Mantan",
+        price: 19000,
+        options: {
+          Temperature: ["Ice", "Hot"],
+          Size: ["Regular", "Large", "Jumbo"],
+          "Sugar Level": ["Normal Sugar", "Less Sugar", "No Sugar"],
+          "Ice Level": ["Normal Ice", "Less Ice", "No Ice"]
+        }
+      },
+      {
+        id: 2,
+        name: "Friend Chip Cookies",
+        price: 17000
+      }
+    ]
   },
   {
-    id: 2,
-    name: "Es Teh",
-    price: 5000,
-    category: "minuman"
+    name: "Janji Jiwa",
+    menu: [
+      {
+        id: 3,
+        name: "Signature Kopi Susu",
+        price: 20000,
+        options: {
+          Temperature: ["Ice", "Hot"],
+          Size: ["Regular", "Large", "Ultimate"],
+          "Ice Level": ["Normal Ice", "Less Ice"],
+          "Sugar Level": ["Normal Sugar", "Less Sugar"]
+        }
+      },
+      {
+        id: 4,
+        name: "Plain Toast",
+        price: 19000
+      }
+    ]
   },
   {
-    id: 3,
-    name: "Nasi Goreng",
-    price: 15000,
-    category: "makanan"
+    name: "Fore",
+    menu: [
+      {
+        id: 5,
+        name: "Kopi Dari Tani",
+        price: 25000,
+        options: {
+          Temperature: ["Ice"],
+          Size: ["Regular Ice", "Large Ice"],
+          "Ice Level": ["Normal Ice", "Less Ice", "More Ice"],
+          "Sweetness Level": ["Normal Sweet", "Less Sweet"]
+        }
+      },
+      {
+        id: 6,
+        name: "Butter Croissant",
+        price: 24000
+      }
+    ]
   }
 ]
